741f8a177be8464426a700a7572c5e83deb07c4e
Vlad@aser-es1-131 <pereskokow@gmail.com>
2016-09-28 22:39:47 +0300

    updated some commands

---
+++ personal/remfils-packages.el
@@ -0,0 +1,10 @@
+;;; Code:
+;;; package --- Summary
+;;; these are additional packages for my emacs
+
+(prelude-require-packages '(multiple-cursors
+			    linum
+			    emmet-mode))
+
+(provide 'remfils-packages)
+;;; remfils-packages.el ends here
---
+++ personal/remfils-settings.el
@@ -0,0 +1,13 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+
+(global-linum-mode 1)
+(setq linum-format " %3d")
+
+;; For number
+(set-face-attribute 'linum nil :height 100)
+(setq visible-bell 1)
+
+(provide 'remfils-settings)
+;;; remfils-settings.el ends here
