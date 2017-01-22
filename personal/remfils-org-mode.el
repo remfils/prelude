233caa6f9fc97f5df96ccae8bfb321c2bebaa8a9
Vlad@aser-es1-131 <pereskokow@gmail.com>
2017-01-22 23:07:18 +0300

    removed ace-jump-mode bcs "C-c j" is on avy

--- personal/remfils-org-mode.el
+++ personal/remfils-org-mode.el
@@ -2,11 +2,5 @@
 ;;; Commentary:
 ;;; Code:
 
-(defun remfils/org-mode-hook ()
-  (define-key org-mode-map (kbd "C-c SPC") nil))
-
-(add-hook 'org-mode-hook 'remfils/org-mode-hook)
-
-
 
 ;;; remfils-org-mode.el ends here
