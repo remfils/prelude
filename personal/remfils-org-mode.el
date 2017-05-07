8372b722bf509b4e0c6b7c84db11d37d998f61f7
vlad@aspire <pereskokow@gmail.com>
2017-05-07 20:09:16 +0300

    added org-capture to the mix

--- personal/remfils-org-mode.el
+++ personal/remfils-org-mode.el
@@ -2,5 +2,15 @@
 ;;; Commentary:
 ;;; Code:
 
+(global-set-key (kbd "C-c c") 'org-capture)
 
+(setq org-directory "~/.org")
+
+(setq org-default-notes-file (concat org-directory "/notes.org"))
+
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline (concat org-directory "/t.org") "Tasks")
+         "* TODO %?\n  %i\n  %a")
+        ("j" "Journal" entry (file+datetree (concat org-directory "/j.org"))
+         "* %T\n%?")))
 ;;; remfils-org-mode.el ends here
