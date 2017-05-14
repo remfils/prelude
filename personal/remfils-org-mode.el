b5920bf37b164d5a0ae0ca92e75a3709896695a2
vlad@aspire <pereskokow@gmail.com>
2017-05-14 23:34:22 +0300

    configured to push local .org repo on function call

--- personal/remfils-org-mode.el
+++ personal/remfils-org-mode.el
@@ -12,9 +12,17 @@
       '(("t" "Todo" entry (file+headline (concat org-directory "/t.org") "Tasks")
          "* TODO %?\n  %i\n  %a")
         ("j" "Journal" entry (file+datetree (concat org-directory "/j.org"))
+         "* %T\n%?")
+        ("l" "Local journal" entry (file+datetree (concat org-directory "/local-j.org"))
          "* %T\n%?")))
 
 (org-babel-do-load-languages
  'org-babel-load-languages '((sh . t) (python . t) (perl . t)))
+
+
+(defun remfils/commit-and-push-org-chanhes()
+  "This function updates repo at org-directory and pushes new changes"
+  (interactive)
+  (shell-command (concat "cd " org-directory " && git stash && git pull && git stash pop && git commit -am '++' && git push")))
 
 ;;; remfils-org-mode.el ends here
