34265384165b4e82463d80957bc4ddf7fbcc1d54
vlad@aspire <pereskokow@gmail.com>
2017-06-18 00:27:11 +0300

    updated remfils/commit-and-push-org-changes fn to use magit-git-command

--- personal/remfils-org-mode.el
+++ personal/remfils-org-mode.el
@@ -22,9 +22,14 @@
  'org-babel-load-languages '((sh . t) (python . t) (perl . t)))
 
 
+(defun remfils/run-git-commands-on-repo (repo-dir commands)
+  (let ((default-directory repo-dir)
+        (git-commands (mapconcat (lambda (s) (concat "git " s)) commands " && ")))
+      (magit-git-command git-commands)))
+
 (defun remfils/commit-and-push-org-changes()
   "This function updates repo at org-directory and pushes new changes"
   (interactive)
-  (shell-command (concat "cd " org-directory " && git stash && git pull && git stash pop && git commit -am '++' && git push")))
+  (remfils/run-git-commands-on-repo "~/.org" '("stash" "pull" "stash pop" "commit -am \"++\"" "push")))
 
 ;;; remfils-org-mode.el ends here
