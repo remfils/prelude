6ab87c3efe8f0eacd59e1884d0f1a7fc014aad93
Vlad@home <pereskokow@gmail.com>
2017-07-31 22:25:36 +0300

    added M-i for helm swoop

--- personal/remfils-init-hotkeys.el
+++ personal/remfils-init-hotkeys.el
@@ -1,6 +1,6 @@
 (cua-mode t)
 
-(global-set-key (kbd "M-i") 'previous-line) ; was tab-to-tab-stop wasd
+;; (global-set-key (kbd "M-i") 'previous-line) ; was tab-to-tab-stop wasd
 (global-set-key (kbd "M-k") 'next-line) ; was kill-sentence
 (global-set-key (kbd "M-j") 'backward-char) ; was indent-new-comment-line
 (global-set-key (kbd "M-l") 'forward-char)
@@ -44,6 +44,24 @@
 (global-set-key (kbd "C-z") 'undo-tree-undo)
 
 (global-set-key (kbd "C-S-z") 'undo-tree-redo)
+
+;; helm swoop
+
+;; Change the keybinds to whatever you like :)
+(global-set-key (kbd "M-i") 'helm-swoop)
+(global-set-key (kbd "M-I") 'helm-swoop-back-to-last-point)
+(global-set-key (kbd "C-c M-i") 'helm-multi-swoop)
+(global-set-key (kbd "C-x M-i") 'helm-multi-swoop-all)
+
+;; When doing isearch, hand the word over to helm-swoop
+(define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
+;; From helm-swoop to helm-multi-swoop-all
+(define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)
+;; When doing evil-search, hand the word over to helm-swoop
+;; (define-key evil-motion-state-map (kbd "M-i") 'helm-swoop-from-evil-search)
+
+;; Instead of helm-multi-swoop-all, you can also use helm-multi-swoop-current-mode
+(define-key helm-swoop-map (kbd "M-m") 'helm-multi-swoop-current-mode-from-helm-swoop)
 
 ;; funcs
 
