93d06cc605a35714066ca0a3c57493ed176b5612
vlad@aspire <pereskokow@gmail.com>
2017-07-31 23:44:24 +0300

    fix: for some reason helm swoop var not def

--- personal/remfils-init-hotkeys.el
+++ personal/remfils-init-hotkeys.el
@@ -55,13 +55,15 @@
 
 ;; When doing isearch, hand the word over to helm-swoop
 (define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
-;; From helm-swoop to helm-multi-swoop-all
-(define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)
-;; When doing evil-search, hand the word over to helm-swoop
-;; (define-key evil-motion-state-map (kbd "M-i") 'helm-swoop-from-evil-search)
 
-;; Instead of helm-multi-swoop-all, you can also use helm-multi-swoop-current-mode
-(define-key helm-swoop-map (kbd "M-m") 'helm-multi-swoop-current-mode-from-helm-swoop)
+(when (boundp 'helm-swoop-map)
+  (progn
+    ;; Instead of helm-multi-swoop-all, you can also use helm-multi-swoop-current-mode    
+    (define-key helm-swoop-map (kbd "M-m") 'helm-multi-swoop-current-mode-from-helm-swoop)
+    ;; From helm-swoop to helm-multi-swoop-all
+    ;; When doing evil-search, hand the word over to helm-swoop
+    ;; (define-key evil-motion-state-map (kbd "M-i") 'helm-swoop-from-evil-search)
+    (define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)))
 
 ;; funcs
 
