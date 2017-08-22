43591392b014c26387999ac8f473f7e216ab7116
vlad@aspire <pereskokow@gmail.com>
2017-08-22 22:28:24 +0300

    updated configs

--- personal/remfils-packages.el
+++ personal/remfils-packages.el
@@ -16,7 +16,6 @@
 
                             helm-css-scss
                             web-beautify
-                            material-theme
 
                             rainbow-delimiters
                             rainbow-mode
