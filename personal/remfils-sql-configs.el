98069744252cd8cd0ee9773ce68d9ea7c9b3631e
Vlad@CFU <pereskokow@gmail.com>
2017-09-25 16:51:20 +0300

    added sql configs

---
+++ personal/remfils-sql-configs.el
@@ -0,0 +1,16 @@
+(add-hook 'sql-interactive-mode-hook
+          (lambda ()
+            (toggle-truncate-lines t)))
+
+
+(setq sql-connection-alist
+      '((work/science-days (sql-product 'postgres)
+                           (sql-port 5432)
+                           (sql-server "10.100.112.25")
+                           (sql-user "sciencedayuser")
+                           (sql-database "SciencedaysDB"))
+        '((work/physteh (sql-product 'mysql)
+                        (sql-server "10.100.112.143")
+                        (sql-user "root")
+                        (sql-database "SciencedaysDB"))
+        ))
