31c95b571389eb43b709edd32bdf3397b9cf4d22
Vlad Pereskokov <pereskokow@gmail.com>
2018-06-23 10:59:42 +0300

    added disable-mouse-mode

--- personal/remfils-settings.el
+++ personal/remfils-settings.el
@@ -78,5 +78,21 @@
 ;; edit-server is an app to edit textareas in firefox
 (edit-server-start)
 
+;; src: http://endlessparentheses.com/disable-mouse-only-inside-emacs.html
+(define-minor-mode disable-mouse-mode
+  "A minor-mode that disables all mouse keybinds."
+  :global t
+  :lighter " 🐭"
+  :keymap (make-sparse-keymap))
+
+(dolist (type '(mouse down-mouse drag-mouse
+                      double-mouse triple-mouse))
+  (dolist (prefix '("" C- M- S- M-S- C-M- C-S- C-M-S-))
+    ;; Yes, I actually HAD to go up to 7 here.
+    (dotimes (n 7)
+      (let ((k (format "%s%s-%s" prefix type n)))
+        (define-key disable-mouse-mode-map
+          (vector (intern k)) #'ignore)))))
+
 (provide 'remfils-settings)
 ;;; remfils-settings.el ends here
