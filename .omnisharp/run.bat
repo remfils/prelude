a55dadb73d24e6b23ba83426514f73aa41a7ee27
Vlad Pereskokov <pereskokow@gmail.com>
2018-07-29 13:54:47 +0300

    [!] updated omnisharp config

--- .omnisharp/run.bat
+++ .omnisharp/run.bat
@@ -1,1 +1,3 @@
-%HOME%\.emacs.d\.omnisharp\1.24\OmniSharp.exe %*
+rem %HOME%\.emacs.d\.omnisharp\omnisharp-roslyn\artifacts\scripts\OmniSharp.Http.cmd %*
+
+C:\home\.emacs.d\.omnisharp\omnisharp-roslyn\artifacts\publish\OmniSharp.Stdio.Driver\win7-x64\OmniSharp.exe %*
