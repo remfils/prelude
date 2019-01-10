ea6a5e11faaede8def46e47ac5f615ac3e6999e6
remfils@win10-hp <remfils@ya.ru>
2019-01-11 01:26:17 +0300

    agenda: added colors

--- personal/remfils-org-mode.el
+++ personal/remfils-org-mode.el
@@ -62,6 +62,25 @@
 (setq org-odt-category-map-alist
       '(("__Figure__" "Изображение" "value" "Изображение" org-odt--enumerable-image-p)))
 
+(add-hook 'org-agenda-finalize-hook
+          `remfils/org-agenda-colors)
+
+(defun remfils/org-agenda-colors()
+      (save-excursion
+        (color-org-header "$$$:" "black" "lightgreen")
+        (color-org-header "work:" "tan" "SaddleBrown")
+        (color-org-header "кукиит:" "bisque" "bisque4")
+        (color-org-header "асп:" "DeepSkyBlue" "white")
+        ))
+
+(defun color-org-header (tag backcolor forecolor)
+  ""
+  (interactive)
+  (goto-char (point-min))
+  (while (re-search-forward tag nil t)
+    (add-text-properties (match-beginning 0) (- (match-end 0) 1)
+                         `(face (:background, backcolor, :foreground, forecolor)))))
+
 
 (defun remfils/run-git-commands-on-repo (repo-dir commands)
   (let ((default-directory repo-dir)
