1d4c21896056b34f2b4e4226a2390a2a73cbde23
remfils@work <remfils@ya.ru>
2019-10-04 10:35:53 +0300

    python3 is default. elpy uses company backends?

--- personal/python.el
+++ personal/python.el
@@ -1,1 +1,7 @@
-(setq elpy-rpc-python-command "python3")
+;(setq elpy-rpc-python-command "python")
+
+
+(add-hook 'elpy-mode-hook
+          (lambda ()
+            (set (make-local-variable 'company-backends)
+                 '((company-dabbrev-code company-yasnippet elpy-company-backend comply-jedi)))))
