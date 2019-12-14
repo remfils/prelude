e4a39668979c64ed3636bd05b40b866cd44897cb
remfils@yeepo <remfils@ya.ru>
2019-12-14 23:30:45 +0300

    f added remfils/copy-file-name-to-clipboard

--- personal/packages/remfils-crux.el
+++ personal/packages/remfils-crux.el
@@ -66,3 +66,14 @@
     (lambda (link)
       (when (string-match "\.png" (org-element-property :path link))
         (org-link-unescape (org-element-property :path link))))))
+
+
+(defun remfils/copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
