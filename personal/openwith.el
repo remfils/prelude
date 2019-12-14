9bca42d234cc659394e41d23ed72a504f4658212
remfils@yeepo <remfils@ya.ru>
2019-12-14 23:31:19 +0300

    openwith on linux

--- personal/openwith.el
+++ personal/openwith.el
@@ -1,20 +1,27 @@
 (require 'openwith)
 
 (setq remfils/programs
-      '(
-        ("writer" . "C:\\Program Files\\LibreOffice\\program\\swriter.exe")
-        ("calc" . "C:\\Program Files\\LibreOffice\\program\\scalc.exe")
-        ))
+      (cond
+       ;; windows
+       ((string-equal system-type "windows-nt")
+        '(("writer" . "C:\\Program Files\\LibreOffice\\program\\swriter.exe")
+          ("calc" . "C:\\Program Files\\LibreOffice\\program\\scalc.exe"))
+        )
+       ;; linux
+       ((string-equal system-type "gnu/linux") ; linux
+        '(("writer" . "libreoffice")
+          ("calc" . "libreoffice"))
+        )))
 
 (setq openwith-associations
       (list
        (list (openwith-make-extension-regexp
               '("doc" "docx" "odt"))
-             (assoc "writer" remfils/programs)
+             (cdr (assoc "writer" remfils/programs))
              '(file))
        (list (openwith-make-extension-regexp
               '("xls" "xlsx" "ods"))
-             (assoc "calc" remfils/programs)
+             (cdr (assoc "calc" remfils/programs))
              '(file))
        ))
 
